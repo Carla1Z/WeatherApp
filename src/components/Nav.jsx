1e4e28c65ed4aee0a2133ee5915de250c42a2910
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-12-07 15:17:52 -0300

    responsive nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,12 @@ import SearchBar from "./SearchBar";
 import icono from "../assets/icono.gif";
 import styles from "./css/Nav.module.css";
 
-function Nav({onSearch}) {
+function Nav({ onSearch }) {
   return (
     <div className={styles.nav}>
-      <img src={icono} alt="icono" />
+      <img src={icono} alt="icono" className={styles.icono} />
       <h2 className={styles.name}>Weather App</h2>
-      <SearchBar onSearch={onSearch}/>
+      <SearchBar onSearch={onSearch} />
     </div>
   );
 }
